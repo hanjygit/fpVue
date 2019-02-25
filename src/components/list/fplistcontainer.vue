1e8da80c14ec09c0164f366fb6d2572c0545aee8
hjy <hjy_sun@163.com>
2019-02-25 16:26:52 +0800

    查询列表表格样式

--- src/components/list/fplistcontainer.vue
+++ src/components/list/fplistcontainer.vue
@@ -2,9 +2,19 @@
 <template>
     <div class="listContent">
         <!--<slot name="listContent"></slot>-->
-        <div class="tbList">
-            <div class="table-container" ref="thwidth">
+        <div class="tableList">
+            <div class="th-container" ref="thwidth">
                 <div v-for="(item, index) in listHeader" :key="index" class="table-header" :style="{width: item.width}">{{item.label}}</div>
+            </div>
+            <div class="tb-container" ref="thwidth">
+                <div v-for="(item, index) in listHeader" :key="index" class="table-body" :style="{width: item.width}" v-if="index==0">单选框</div>
+                <div v-for="(item, index) in listHeader" :key="index" class="table-body" :style="{width: item.width}" v-if="index==1">{{index}}</div>
+                <div v-for="(item, index) in listHeader" :key="index" class="table-body" :style="{width: item.width}" v-if="index>1">{{item.column}}</div>
+            </div>
+            <div class="tb-container" ref="thwidth">
+                <div v-for="(item, index) in listHeader" :key="index" class="table-body" :style="{width: item.width}" v-if="index==0">单选框</div>
+                <div v-for="(item, index) in listHeader" :key="index" class="table-body" :style="{width: item.width}" v-if="index==1">{{index}}</div>
+                <div v-for="(item, index) in listHeader" :key="index" class="table-body" :style="{width: item.width}" v-if="index>1">{{item.column}}</div>
             </div>
         </div>
         <div class="footerList"></div>
@@ -38,24 +48,51 @@ export default {
     .listContent{
         position: relative
     }
-    .tbList{
-        width: 100%;
+    .tableList{
+        width: auto;
+        min-width: 100%;
+        height: auto;
+        min-height: 310px;
         background-color: #fff;
         margin-top: 10px;
         border: 1px solid #DDDDDD;
-        height: 340px;
         overflow: auto;
+        white-space: nowrap;
     }
-    .table-container{
-        width: auto
-    }
-    .table-header{
-        min-width: 40px;
+    .th-container{
+        width: auto;
         height: 40px;
         line-height: 40px;
-        border-bottom: 1px solid #DDDDDD;
+        font-size: 0;
+        text-align: center;
+        color: #828995;
+    }
+    .table-header{
+        height: 100%;
+        border-bottom: 1px solid #f2f3f5;
         border-right: 1px solid #DDDDDD;
         display: inline-block;
         vertical-align: middle;
+        font-size: 14px;
+        background-color:#f2f3f5;
+    }
+    .tb-container{
+        width: auto;
+        height: 40px;
+        line-height: 40px;
+        font-size: 0;
+        text-align: center;
+        color: #495362;
+    }
+    .table-body{
+        height: 100%;
+        border-bottom: 1px solid #f2f3f5;
+        border-right: 1px solid #DDDDDD;
+        display: inline-block;
+        vertical-align: middle;
+        font-size: 14px;
+    }
+    .tb-container:nth-child(odd) .table-body{
+        background-color:#f2f3f5;
     }
 </style>
